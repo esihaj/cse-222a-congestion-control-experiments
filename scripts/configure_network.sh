b2332cc1b25a567851c33e54a770d680cd9f5e7a
Ehsan Hajyasini <ehajyasini@ucsd.edu>
2024-12-09 10:47:19 -0800

    update configure network to set qdisc

--- scripts/configure_network.sh
+++ scripts/configure_network.sh
@@ -10,6 +10,17 @@ LOSS="$4"
 echo -e "\e[32m[CONFIGURE_NETWORK] Setting CCA to $CCA\e[0m"
 sudo sysctl -w net.ipv4.tcp_congestion_control="$CCA"
 
+# Set the default qdisc based on the CCA
+if [ "$CCA" == "bbr" ]; then
+    QDISC="fq"
+else
+    QDISC="fq_codel"
+fi
+
+echo -e "\e[32m[CONFIGURE_NETWORK] Setting net.core.default_qdisc to $QDISC\e[0m"
+sudo sysctl -w net.core.default_qdisc="$QDISC"
+
+
 # Clear existing qdisc (ignore error if no qdisc)
 echo -e "\e[32m[CONFIGURE_NETWORK] Clearing existing qdisc on $IFACE\e[0m"
 sudo tc qdisc del dev "$IFACE" root 2>/dev/null || true
@@ -23,6 +34,11 @@ if [ "$DELAY" != "0" ] || [ "$LOSS" != "0" ]; then
     if [ "$LOSS" != "0" ]; then
         NETEM_CMD="$NETEM_CMD loss ${LOSS}%"
     fi
+    NETEM_CMD="$NETEM_CMD limit 5000"
     echo -e "\e[32m[CONFIGURE_NETWORK] Applying netem: $NETEM_CMD\e[0m"
     eval "$NETEM_CMD"
+else
+    # Apply default qdisc if no netem settings are applied
+    echo -e "\e[32m[CONFIGURE_NETWORK] Applying default qdisc: $QDISC on $IFACE\e[0m"
+    sudo tc qdisc add dev "$IFACE" root "$QDISC"
 fi
