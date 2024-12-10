1a18217a9925df1875d45344692ee0a66c8a2496
Ehsan Hajyasini <ehajyasini@ucsd.edu>
2024-12-10 10:11:16 -0800

    update run_experiments.sh

--- run_experiments.sh
+++ run_experiments.sh
@@ -25,38 +25,36 @@ ssh $SSH_OPTS $SSH_USER@$SERVER_IP "rm -rf results || true"
 echo "[RUN_EXPERIMENTS] Clearing out old results on client..."
 ssh $SSH_OPTS $SSH_USER@$CLIENT_IP "rm -rf results || true"
 
-scp $SSH_OPTS scripts/remote_runner.sh $SSH_USER@$SERVER_IP:remote_runner.sh
-ssh $SSH_OPTS $SSH_USER@$SERVER_IP "chmod +x remote_runner.sh"
+scp $SSH_OPTS scripts/{remote_setup.sh,remote_runner.sh} $SSH_USER@$SERVER_IP:
+ssh $SSH_OPTS $SSH_USER@$SERVER_IP "chmod +x remote_runner.sh remote_setup.sh"
 
 scp $SSH_OPTS scripts/{remote_runner,remote_setup,configure_network,benchmark_nginx}.sh $SSH_USER@$CLIENT_IP:
 ssh $SSH_OPTS $SSH_USER@$CLIENT_IP "chmod +x remote_runner.sh remote_setup.sh configure_network.sh benchmark_nginx.sh"
 
 #remove results dir 
 echo "[RUN_EXPERIMENTS] Clearing out old local results..."
-rm -rf results
 
 echo "[RUN_EXPERIMENTS] Running scenarios..."
 
 SCENARIOS=(
-    "cubic 0 0.0 results/cubic/standard"
-    "cubic 20 0.0 results/cubic/delay_20ms"
+    "cubic 0 0 results/cubic/standard"
+    "cubic 20 0 results/cubic/delay_20ms"
     "cubic 20 0.005 results/cubic/delay_20ms_loss_0.005"
     "cubic 20 0.01 results/cubic/delay_20ms_loss_0.01"
-    "bbr 0 0.0 results/bbr/standard"
-    "bbr 20 0.0 results/bbr/delay_20ms"
+    "bbr 0 0 results/bbr/standard"
+    "bbr 20 0 results/bbr/delay_20ms"
     "bbr 20 0.005 results/bbr/delay_20ms_loss_0.005"
     "bbr 20 0.01 results/bbr/delay_20ms_loss_0.01"
 )
 
-DURATION=30
+DURATION=150
 
 for scenario in "${SCENARIOS[@]}"; do
     ./run_scenario.sh "$SERVER_IP" "$CLIENT_IP" $scenario $DURATION
     echo "----------------------------------------------------------"
     echo "----------------------------------------------------------"
 done
-
-
+./scripts/plot_aggregate_metrics.py results/
 
 ./run_nginx_scenarios.sh
 
